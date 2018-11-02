a06e138a4c34475bec5c35fdec9d977f177d80a6
PyJohn81 <pyjohn81@gmail.com>
2018-11-02 10:31:22 +0800

    Removed unneeded loop.

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ from flask import Flask , render_template
 app = Flask(__name__)
 
 infoNames = info.quickInfo()
-global infoNames2 #infoNames2 is a variable that stores every index in the list with added <h1> tags
-for i in range(len(infoNames)): #this loop adds the <h1> tag for every index in the list and appends it to the infoNames2 variable
-    if (i == 0): #this excepts the profession data from the list because it will undergo a different process.
-        pass
-    elif (i == 1): #this creates the first <h1> tag in the infoNames2 variable
-        infoNames2 = '<h1>{}</h1>'.format(infoNames[i])
-    elif (i > 1): #this appends all the other indexes in the list to infoNames2 after including the <h1> tag
-        infoNames2 = infoNames2 + '<h1>{}</h1>'.format(infoNames[i])
 
-profName = infoNames[0] #extracts out the profName
+profName = infoNames[0] #extracts out the profName b4 deletion
 del infoNames[0]    #deletes the profName
 
 @app.route('/')
