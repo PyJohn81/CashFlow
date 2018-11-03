80521beea176c8eb765d811498921d3e48fa8c92
PyJohn81 <pyjohn81@gmail.com>
2018-11-03 12:33:55 +0800

    Updated with more style.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from AppPack import info
-from flask import Flask , render_template
+from flask import Flask , render_template , url_for
 app = Flask(__name__)
 
 profName = info.index[0] #extracts out the profName b4 deletion
