43eef8992d19d8b4f46ac1b5988abaac78fcb786
PyJohn81 <pyjohn81@gmail.com>
2018-11-03 11:38:08 +0800

    Added bootstrap

--- app.py
+++ app.py
@@ -11,6 +11,5 @@ def output():
     return render_template('quickInfo.html' , profName=profName , index=info.index)
 
 
-
 if (__name__ == '__main__'):
     app.run(debug=True,host='0.0.0.0',port='8000')
